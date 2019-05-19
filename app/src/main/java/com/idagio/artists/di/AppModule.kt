b4822162c7511bba30e4458b3e6496f8234fe3cf
Mnatsakan Abgaryan <matso.abgaryan@mail.ru>
2019-05-19 16:04:02 +0200

    Dagger AppModule added

---
+++ app/src/main/java/com/idagio/artists/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.idagio.artists.di
+
+import androidx.lifecycle.ViewModelProvider
+import com.idagio.artists.repository.ArtistsDataSource
+import com.idagio.artists.view.ViewModelFactory
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module(subcomponents = [ViewModelSubComponent::class])
+internal class AppModule {
+    @Singleton
+    @Provides
+    fun provideArtistsDataSource(): ArtistsDataSource {
+
+        return Retrofit.Builder()
+            .baseUrl(ArtistsDataSource.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(ArtistsDataSource::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideViewModelFactory(
+        viewModelSubComponent: ViewModelSubComponent.Builder
+    ): ViewModelProvider.Factory {
+
+        return ViewModelFactory(viewModelSubComponent.build())
+    }
+}
