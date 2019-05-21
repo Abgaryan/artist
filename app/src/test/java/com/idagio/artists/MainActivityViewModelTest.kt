28fdd701f51e4def48bf8742588bb305d1bbdfa5
Mnatsakan Abgaryan <matso.abgaryan@mail.ru>
2019-05-21 19:13:32 +0200

    MainActivityViewModel test fixes

--- app/src/test/java/com/idagio/artists/MainActivityViewModelTest.kt
+++ app/src/test/java/com/idagio/artists/MainActivityViewModelTest.kt
@@ -1,16 +1,19 @@
 package com.idagio.artists
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.idagio.artists.ArtistsRepositoryTest.Companion.TERM_BA
 import com.idagio.artists.ArtistsRepositoryTest.Companion.TERM_BAC
 import com.idagio.artists.ArtistsRepositoryTest.Companion.TERM_BACH
 import com.idagio.artists.ArtistsRepositoryTest.Companion.TERM_EMPTY
 import com.idagio.artists.repository.ArtistsRepository
+import com.idagio.artists.schedulers.TrampolineSchedulerProvider
 import com.idagio.artists.view.ui.artist_list.MainActivityViewModel
-
 import io.reactivex.Single
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
@@ -22,6 +25,8 @@ import org.mockito.MockitoAnnotations
  */
 class MainActivityViewModelTest {
 
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
 
     @Mock
     private lateinit var mockRepository: ArtistsRepository
@@ -29,6 +34,8 @@ class MainActivityViewModelTest {
     private lateinit var mainActivityViewModel: MainActivityViewModel
 
     private val artistResultWrapperFactory = ArtistResultWrapperFactory()
+
+
 
     @Before
     fun setUp() {
@@ -39,34 +46,42 @@ class MainActivityViewModelTest {
         Mockito.`when`(mockRepository.searchArtists(TERM_EMPTY)).thenReturn(
             Single.just(artistResultWrapperFactory.artistsResultWrapperWithoutPerson())
         )
-        mainActivityViewModel = MainActivityViewModel(mockRepository)
+        mainActivityViewModel = MainActivityViewModel(mockRepository,
+            TrampolineSchedulerProvider()
+        )
     }
 
     @Test
     fun test_empty_search() {
         mainActivityViewModel.searchArtist(TERM_EMPTY)
+        Thread.sleep(1000)
         Assert.assertEquals(mainActivityViewModel.persons.value, null)
     }
 
     @Test
     fun test_search_with_two_characters() {
         mainActivityViewModel.searchArtist(TERM_BA)
+        Thread.sleep(1000)
         Assert.assertEquals(mainActivityViewModel.persons.value, null)
     }
 
     @Test
     fun test_valid_search() {
         mainActivityViewModel.searchArtist(TERM_BACH)
+        Thread.sleep(1000)
         Assert.assertEquals(
             mainActivityViewModel.persons.value,
             artistResultWrapperFactory.artistsResultWrapperWithTwoPersons().artists.persons
         )
+
     }
 
     @Test
     fun test_search_debounce() {
         mainActivityViewModel.searchArtist(TERM_BAC)
+        Thread.sleep(200)
         mainActivityViewModel.searchArtist(TERM_BACH)
+        Thread.sleep(1000)
         Assert.assertEquals(
             mainActivityViewModel.persons.value,
             artistResultWrapperFactory.artistsResultWrapperWithTwoPersons().artists.persons
